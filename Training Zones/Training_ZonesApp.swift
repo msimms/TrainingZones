ee5031a517af29e7f8501725f9c2ba587d7e334a
Mike Simms <msimms01@gmail.com>
2023-05-25 12:58:00 -0400

    Initial Commit

---
+++ Training Zones/Training_ZonesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Training_ZonesApp.swift
+//  Training Zones
+//
+//  Created by Michael Simms on 5/25/23.
+//
+
+import SwiftUI
+
+@main
+struct Training_ZonesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
