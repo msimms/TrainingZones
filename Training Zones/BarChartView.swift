1481a8193992735a41e430680fc108ab0b626603
Mike Simms <msimms01@gmail.com>
2023-06-13 11:52:13 -0400

    Bar chart cleanup.

--- Training Zones/BarChartView.swift
+++ Training Zones/BarChartView.swift
@@ -17,6 +17,7 @@ struct BarChartView: View {
 	let max: Double
 	let color: Color
 	let units: String
+	private var description: String = ""
 	@State private var showsAlert = false
 
 	init(bars: [Bar], color: Color, units: String) {
@@ -24,29 +25,36 @@ struct BarChartView: View {
 		self.max = bars.map { $0.value }.max() ?? 0
 		self.color = color
 		self.units = units
+		var zoneNum = 1
+		for bar in bars {
+			self.description += "Zone "
+			self.description += String(zoneNum)
+			self.description += " : "
+			self.description += bar.description
+			self.description += "\n"
+			zoneNum += 1
+		}
 	}
 
 	var body: some View {
 		GeometryReader { geometry in
 			HStack(alignment: .bottom) {
 				ForEach(self.bars) { bar in
-					VStack(alignment: .center) {
-						ZStack() {
-							Rectangle()
-								.frame(height: CGFloat(bar.value) / CGFloat(self.max) * geometry.size.height)
-								.overlay(Rectangle().stroke(self.color).background(self.color))
-								.accessibility(label: Text(bar.label))
-								.onTapGesture {
-									self.showsAlert = true
-								}
-							Text(bar.label + " " + self.units)
-								.font(.title3)
-								.rotationEffect(Angle(degrees: -90))
-								.offset(y: 0)
-								.alert(isPresented: self.$showsAlert) { () -> Alert in
-									Alert(title: Text("Description"), message: Text(bar.description))}
-						}
+					ZStack() {
+						Rectangle()
+							.frame(height: CGFloat(bar.value) / CGFloat(self.max) * geometry.size.height)
+							.overlay(Rectangle().stroke(self.color).background(self.color))
+							.accessibility(label: Text(bar.label + " " + self.units))
+							.onTapGesture {
+								self.showsAlert = true
+							}
+						Text(bar.label + " " + self.units)
+							.font(.title3)
+							.rotationEffect(Angle(degrees: -90))
+							.offset(y: 0)
 					}
+					.alert(isPresented: self.$showsAlert) { () -> Alert in
+						Alert(title: Text("Description"), message: Text(self.description))}
 				}
 			}
 		}
